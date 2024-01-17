ef6b317acbe83040a362914ae55e637435663c5d
Jordan Rafael <61181764+jordanrafaell@users.noreply.github.com>
2024-01-17 08:20:17 -0300

    App terminado

---
+++ Calculo-de-Celsius/script.js
@@ -0,0 +1,17 @@
+const celsius = document.getElementById('celsius');
+const fahrenheit = document.getElementById('fahrenheit');
+
+window.addEventListener('load', () => celsius.focus());
+
+celsius.addEventListener("input", () => {
+    fahrenheit.value = ((celsius.value * 9) / 5 + 32).toFixed(2);
+
+    if (!celsius.value) fahrenheit.value = "";
+})
+
+fahrenheit.addEventListener("input", () => {
+    celsius.value = ((fahrenheit.value * 32) / 5 + 9).toFixed(2);
+    
+    if (!fahrenheit.value) celsius.value = "";
+})
+
